1c78c25f89d0d82d2880774fb0665709aaeccfea
kusnadin-ali <kusnadin.ali.12@gmail.com>
2023-10-05 21:17:17 +0700

    dashboard screen finished

---
+++ Front-End/tmshub/lib/src/screens/penggajian_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PenggajianScreen extends StatefulWidget {
+  const PenggajianScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PenggajianScreen> createState() => _PenggajianScreenState();
+}
+
+class _PenggajianScreenState extends State<PenggajianScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ Front-End/tmshub/lib/src/screens/presensi_screen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class PresensiScreen extends StatefulWidget {
+  const PresensiScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PresensiScreen> createState() => _PresensiScreenState();
+}
+
+class _PresensiScreenState extends State<PresensiScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Presensi Screen");
+  }
+}
