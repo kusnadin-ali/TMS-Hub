6941bdfb0a14c4b815eaa7970698c81a6a65f0d8
kusnadin-ali <kusnadin.ali.12@gmail.com>
2023-10-18 19:32:22 +0700

    add be template

---
+++ Back-End/tmshub-be/app/Models/Penggajian.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Penggajian extends Model
+{
+    protected $table = 'penggajian';
+    protected $primaryKey = 'id_penggajian';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_user',
+        'gaji_pokok',
+        'transportasi',
+        'status_gaji',
+        'id_admin',
+        'keterangan',
+        'bonus'
+    ];
+}
